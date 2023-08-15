6e6fedbcad112d1c0be022b783e96b32ba54cd19
jriverosesma <jriverosesma@gmail.com>
2023-08-15 13:53:49 +0200

    test: replace unit-test by pytest

--- test/test_mp3.py
+++ test/test_mp3.py
@@ -1,76 +1,81 @@
 import io
 import os
 import shutil
-import unittest
 
+import pytest
 from mutagen.mp3 import HeaderNotFoundError
 from PIL import Image
 
 from mp3_autotagger.mp3 import MP3
 
 
-class TestMP3(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestMP3, cls).setUpClass()
-        cls.media_dir = "media"
-        cls.tmp_filepath = os.path.join(cls.media_dir, "tmp.mp3")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.remove(cls.tmp_filepath)
-
-    # .mp3 with empty ID3 tags
-    def test_example_0(self):
-        shutil.copyfile(os.path.join(self.media_dir, "example_0.mp3"), self.tmp_filepath)
-        track = MP3(self.tmp_filepath)
-        track.update_tags_shazam()
-        track.save_as(self.tmp_filepath)
-        new_track = MP3(self.tmp_filepath)
-        cover = Image.open(io.BytesIO(new_track.tags["APIC"]))
-        tmp_cover_filepath = os.path.join(self.media_dir, "tmp_cover.png")
-        cover.save(tmp_cover_filepath)
-
-        assert new_track.tags["TPE1"] == "RED HOT CHILI PEPPERS"
-        assert new_track.tags["TIT2"] == "Scar Tissue"
-        assert new_track.tags["TALB"] == "Californication"
-        assert new_track.tags["TCON"] == "Alternative"
-        assert new_track.tags["TDRC"] == "1999"
-        assert new_track.tags["APIC"] != b""
-        assert os.path.exists(self.tmp_filepath)
-        assert os.path.exists(tmp_cover_filepath)
-
-        os.remove(tmp_cover_filepath)
-
-    # .mp3 with filled ID3 tags (no replace)
-    def test_example_1(self):
-        shutil.copyfile(os.path.join(self.media_dir, "example_1.mp3"), self.tmp_filepath)
-        track = MP3(self.tmp_filepath)
-        assert track.tags["TPE1"] == "CREEDENCE CLEARWATER REVIVAL"
-        assert track.tags["TIT2"] == "Suzie Q"
-        assert track.tags["TALB"] == ""
-        assert track.tags["TCON"] == "CLASICOS"
-        assert track.tags["TDRC"] == "1968"
-        assert track.tags["APIC"] != b""
-
-        track.update_tags_shazam(replace_info=False)
-        track.save_as(self.tmp_filepath)
-        new_track = MP3(self.tmp_filepath)
-
-        assert new_track.tags["TPE1"] == "CREEDENCE CLEARWATER REVIVAL"
-        assert new_track.tags["TIT2"] == "Suzie Q"
-        assert new_track.tags["TALB"] == "Creedence Clearwater Revival"
-        assert new_track.tags["TCON"] == "CLASICOS"
-        assert new_track.tags["TDRC"] == "1968"
-        assert new_track.tags["APIC"] != b""
-        assert os.path.exists(self.tmp_filepath)
-
-    # Corrupted .mp3 file
-    def test_example_2(self):
-        shutil.copyfile(os.path.join(self.media_dir, "example_2.mp3"), self.tmp_filepath)
-        with self.assertRaises(HeaderNotFoundError):
-            MP3(self.tmp_filepath)
+@pytest.fixture(scope="class")
+def setup_class_data():
+    media_dir = "test/assets"
+    tmp_filepath = os.path.join(media_dir, "tmp.mp3")
+    yield media_dir, tmp_filepath
+    os.remove(tmp_filepath)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_example_0(setup_class_data):
+    media_dir, tmp_filepath = setup_class_data
+    shutil.copyfile(os.path.join(media_dir, "example_0.mp3"), tmp_filepath)
+    track = MP3(tmp_filepath)
+    track.update_tags_shazam()
+    track.save_as(tmp_filepath)
+    new_track = MP3(tmp_filepath)
+    cover = Image.open(io.BytesIO(new_track.tags["APIC"]))
+    tmp_cover_filepath = os.path.join(media_dir, "tmp_cover.png")
+    cover.save(tmp_cover_filepath)
+
+    assert new_track.tags["TPE1"] == "RED HOT CHILI PEPPERS"
+    shutil.copyfile(os.path.join(media_dir, "example_0.mp3"), tmp_filepath)
+    track = MP3(tmp_filepath)
+    track.update_tags_shazam()
+    track.save_as(tmp_filepath)
+    new_track = MP3(tmp_filepath)
+    cover = Image.open(io.BytesIO(new_track.tags["APIC"]))
+    tmp_cover_filepath = os.path.join(media_dir, "tmp_cover.png")
+    cover.save(tmp_cover_filepath)
+
+    assert new_track.tags["TPE1"] == "RED HOT CHILI PEPPERS"
+    assert new_track.tags["TIT2"] == "Scar Tissue"
+    assert new_track.tags["TALB"] == "Californication (Deluxe Edition)"
+    assert new_track.tags["TCON"] == "Alternative"
+    assert new_track.tags["TDRC"] == "1999"
+    assert new_track.tags["APIC"] != b""
+    assert os.path.exists(tmp_filepath)
+    assert os.path.exists(tmp_cover_filepath)
+
+    os.remove(tmp_cover_filepath)
+
+
+def test_example_1(setup_class_data):
+    media_dir, tmp_filepath = setup_class_data
+    shutil.copyfile(os.path.join(media_dir, "example_1.mp3"), tmp_filepath)
+    track = MP3(tmp_filepath)
+    assert track.tags["TPE1"] == "CREEDENCE CLEARWATER REVIVAL"
+    assert track.tags["TIT2"] == "Suzie Q"
+    assert track.tags["TALB"] == ""
+    assert track.tags["TCON"] == "CLASICOS"
+    assert track.tags["TDRC"] == "1968"
+    assert track.tags["APIC"] != b""
+
+    track.update_tags_shazam(replace_info=False)
+    track.save_as(tmp_filepath)
+    new_track = MP3(tmp_filepath)
+
+    assert new_track.tags["TPE1"] == "CREEDENCE CLEARWATER REVIVAL"
+    assert new_track.tags["TIT2"] == "Suzie Q"
+    assert new_track.tags["TALB"] == "Creedence Clearwater Revival"
+    assert new_track.tags["TCON"] == "CLASICOS"
+    assert new_track.tags["TDRC"] == "1968"
+    assert new_track.tags["APIC"] != b""
+    assert os.path.exists(tmp_filepath)
+
+
+def test_example_2(setup_class_data):
+    media_dir, tmp_filepath = setup_class_data
+    shutil.copyfile(os.path.join(media_dir, "example_2.mp3"), tmp_filepath)
+    with pytest.raises(HeaderNotFoundError):
+        MP3(tmp_filepath)
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,9 +1,9 @@
 import asyncio
 import os
 import shutil
-import unittest
 import urllib
 
+import pytest
 from git.cmd import Git
 
 from mp3_autotagger.utils import (
@@ -14,62 +14,74 @@ from mp3_autotagger.utils import (
 )
 
 
-class TestUtilsShazam(unittest.TestCase):
-    def test_shazam_find_track_info(self):
-        shazam_out = asyncio.run(shazam_find_track_info("media/example_0.mp3"))
-        print(shazam_out["track"]["images"]["coverarthq"])
-        assert shazam_out["track"]["subtitle"].upper() == "RED HOT CHILI PEPPERS"
-        assert shazam_out["track"]["title"] == "Scar Tissue"
-        assert shazam_out["track"]["sections"][0]["metadata"][0]["text"] == "Californication"
-        assert shazam_out["track"]["genres"]["primary"] == "Alternative"
-        assert shazam_out["track"]["sections"][0]["metadata"][2]["text"] == "1999"
-        assert urllib.request.urlopen(shazam_out["track"]["images"]["coverarthq"]).status == 200  # OK
+@pytest.fixture(scope="class")
+def media_dir():
+    return "test/assets"
 
 
-class TestUtilsYoutube(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestUtilsYoutube, cls).setUpClass()
-        cls.media_dir = "media"
-        cls.best_audiostream = get_youtube_audiostreams(
-            "https://www.youtube.com/watch?v=HHjSdy9l7Kc", get_best_audio=True
-        )
-        cls.audiostreams = get_youtube_audiostreams("https://www.youtube.com/watch?v=HHjSdy9l7Kc")
-        cls.mp3_filepath = os.path.join(cls.media_dir, "tmp.mp3")
-        cls.webm_filepath = os.path.join(cls.media_dir, "tmp.webm")
-
-    @classmethod
-    def tearDownClass(cls):
-        super(TestUtilsYoutube, cls).setUpClass()
-        os.remove(cls.mp3_filepath)
-        os.remove(cls.webm_filepath)
-
-    def test_get_youtube_audiostreams(self):
-        assert max([float(a.bitrate.replace("k", "e3")) for a in self.audiostreams]) == float(
-            self.best_audiostream.bitrate.replace("k", "e3")
-        )
-        assert self.best_audiostream.extension == "webm"
-        print(self.best_audiostream.filename)
-        assert self.best_audiostream.filename == "15 Seconds.webm"
-        assert self.best_audiostream.bitrate == "132.155k"
-        assert self.best_audiostream.get_filesize() == 248138
-
-    def test_download_youtube_audiostream(self):
-        download_youtube_audiostream(self.best_audiostream, save_filepath=self.webm_filepath)
-        assert os.path.exists(self.webm_filepath)
-
-    def test_convert2mp3(self):
-        shutil.copy(os.path.join(self.media_dir, "example.webm"), self.webm_filepath)
-        convert2mp3(self.webm_filepath, self.best_audiostream.extension)
-        assert os.path.exists(self.mp3_filepath)
-        assert not os.path.exists(self.webm_filepath)
+@pytest.fixture(scope="class")
+def best_audiostream():
+    return get_youtube_audiostreams("https://www.youtube.com/watch?v=HHjSdy9l7Kc", get_best_audio=True)
 
 
-class TestUtilsUpdate(unittest.TestCase):
-    def test_update_app(self):
-        status = Git().fetch("https://github.com/jriverosesma/mp3-autotagger", "main")
-        assert status is not None
+@pytest.fixture(scope="class")
+def audiostreams():
+    return get_youtube_audiostreams("https://www.youtube.com/watch?v=HHjSdy9l7Kc")
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture(scope="class")
+def mp3_filepath(media_dir):
+    return os.path.join(media_dir, "tmp.mp3")
+
+
+@pytest.fixture(scope="class")
+def webm_filepath(media_dir):
+    return os.path.join(media_dir, "tmp.webm")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def cleanup(mp3_filepath, webm_filepath):
+    yield  # This makes sure the cleanup happens after tests run
+    if os.path.exists(mp3_filepath):
+        os.remove(mp3_filepath)
+    if os.path.exists(webm_filepath):
+        os.remove(webm_filepath)
+
+
+def test_shazam_find_track_info():
+    shazam_out = asyncio.run(shazam_find_track_info("media/example_0.mp3"))
+    print(shazam_out["track"]["images"]["coverarthq"])
+    assert shazam_out["track"]["subtitle"].upper() == "RED HOT CHILI PEPPERS"
+    assert shazam_out["track"]["title"] == "Scar Tissue"
+    assert shazam_out["track"]["sections"][0]["metadata"][0]["text"] == "Californication (Deluxe Edition)"
+    assert shazam_out["track"]["genres"]["primary"] == "Alternative"
+    assert shazam_out["track"]["sections"][0]["metadata"][2]["text"] == "1999"
+    assert urllib.request.urlopen(shazam_out["track"]["images"]["coverarthq"]).status == 200  # OK
+
+
+def test_get_youtube_audiostreams(best_audiostream, audiostreams):
+    assert max([float(a.bitrate.replace("k", "e3")) for a in audiostreams]) == float(
+        best_audiostream.bitrate.replace("k", "e3")
+    )
+    assert best_audiostream.extension == "webm"
+    print(best_audiostream.filename)
+    assert best_audiostream.filename == "15 Seconds.webm"
+    assert best_audiostream.bitrate == "132.155k"
+    assert best_audiostream.get_filesize() == 248138
+
+
+def test_download_youtube_audiostream(best_audiostream, webm_filepath):
+    download_youtube_audiostream(best_audiostream, save_filepath=webm_filepath)
+    assert os.path.exists(webm_filepath)
+
+
+def test_convert2mp3(media_dir, best_audiostream, mp3_filepath, webm_filepath):
+    shutil.copy(os.path.join(media_dir, "example.webm"), webm_filepath)
+    convert2mp3(webm_filepath, best_audiostream.extension)
+    assert os.path.exists(mp3_filepath)
+    assert not os.path.exists(webm_filepath)
+
+
+def test_update_app():
+    status = Git().fetch("https://github.com/jriverosesma/mp3-autotagger", "main")
+    assert status is not None
