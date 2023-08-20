47cc82baff834ad843fd1420ecc2b213db484017
jriverosesma <jriverosesma@gmail.com>
2023-08-20 04:32:39 +0200

    chore: minor modification in `setup_win.bat` script

--- scripts/setup_win.bat
+++ scripts/setup_win.bat
@@ -1,6 +1,6 @@
 conda create -y -n mp3-autotagger python=3.10.12 --no-default-packages
 call conda activate mp3-autotagger
-call conda install ffmpeg
+call conda install -y ffmpeg
 python -m pip install --upgrade pip
 pip install mp3-autotagger
 call conda deactivate
