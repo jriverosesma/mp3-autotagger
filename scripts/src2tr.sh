51f44c6a975a72fb4a3f784938655a74ba85d7a2
jriverosesma <jriverosesma@gmail.com>
2023-09-13 23:51:09 +0200

    fix: source code to translation code script

--- scripts/src2tr.sh
+++ scripts/src2tr.sh
@@ -5,7 +5,7 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate mp3-autotagger
 
 # Generate .ts file
-pylupdate5 mp3_autotagger/*.py -ts translations/eng-es.ts
+pylupdate5  mp3_autotagger/gui/*.py mp3_autotagger/utils/*.py -ts translations/eng-es.ts
 
 # Deactivate the conda environment
 conda deactivate
