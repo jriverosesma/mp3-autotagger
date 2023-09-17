139ba81e2ed1d402c3267b8bd2113d9d787dedf4
jriverosesma <jriverosesma@gmail.com>
2023-09-18 00:54:54 +0200

    style: docstrings

--- mp3_autotagger/utils/package.py
+++ mp3_autotagger/utils/package.py
@@ -2,6 +2,8 @@ import sys
 
 
 def is_running_as_pyinstaller_bundle() -> bool:
-    """Check if application is running as a `PyInstaller` bundle."""
+    """
+    Check if application is running as a `PyInstaller` bundle.
+    """
 
     return hasattr(sys, "_MEIPASS")
